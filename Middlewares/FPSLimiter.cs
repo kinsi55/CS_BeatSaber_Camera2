7495b262e67aeb6c9e9a660bf0e54714949d83aa
Kinsi <kinsi55@users.noreply.github.com>
2021-02-01 03:04:32 +0100

    Make FPS limiter more accurately fullfill the target FPS

--- Middlewares/FPSLimiter.cs
+++ Middlewares/FPSLimiter.cs
@@ -1,15 +1,31 @@
 ﻿using Camera2.Interfaces;
+using Newtonsoft.Json;
 
 namespace Camera2.Configuration {
 	class Settings_FPSLimiter {
-		public int limit = 60;
+		private int _limit = 60;
+		[JsonIgnore]
+		public float frameTime { get; private set; } = 1f / 60f;
+
+		public int limit {
+			get { return _limit; }
+			set {
+				frameTime = 1f / value;
+				_limit = value;
+			}
+		}
 	}
 }
 
 namespace Camera2.Middlewares {
 	class FPSLimiter : CamMiddleware, IMHandler {
+		float renderTimeRollAccu = 0f;
+		
 		new public bool Pre() {
-			if(enabled && cam.timeSinceLastRender < 1f / settings.FPSLimiter.limit) return false;
+			if(!enabled) return true;
+
+			if(cam.timeSinceLastRender + renderTimeRollAccu < settings.FPSLimiter.frameTime) return false;
+			renderTimeRollAccu = (cam.timeSinceLastRender + renderTimeRollAccu) % settings.FPSLimiter.frameTime;
 			return true;
 		}
 	}
