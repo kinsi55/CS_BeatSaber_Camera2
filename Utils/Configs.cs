ef4f732f8202b01eb63bf62786a4423ee550a382
Kinsi <kinsi55@users.noreply.github.com>
2021-02-12 21:58:24 +0100

    Minor refactoring

--- Utils/Configs.cs
+++ Utils/Configs.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using IPA.Utilities;
+using System;
 using System.IO;
 
 namespace Camera2.Utils {
 	static class ConfigUtil {
-		public static readonly string ConfigDir = Path.Combine(Environment.CurrentDirectory, "UserData/Camera2");
+		public static readonly string ConfigDir = Path.Combine(UnityGame.UserDataPath, "Camera2");
 		public static readonly string ScenesCfg = Path.Combine(ConfigDir, "Scenes.json");
 		public static readonly string CamsDir = Path.Combine(ConfigDir, "Cameras");
 		public static readonly string MovementScriptsDir = Path.Combine(ConfigDir, "MovementScripts");
