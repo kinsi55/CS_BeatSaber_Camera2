a5a92bcbe6e24619699cccfd177113776b1786ec
Kinsi <kinsi55@users.noreply.github.com>
2021-02-17 04:24:50 +0100

    Fix Transparent / Hidden walls when Bloom is off in the game

--- HarmonyPatches/TransparentWalls.cs
+++ HarmonyPatches/TransparentWalls.cs
@@ -5,9 +5,16 @@ using Camera2.Utils;
 namespace Camera2.HarmonyPatches {
 	[HarmonyPatch(typeof(StretchableObstacle), nameof(StretchableObstacle.SetSizeAndColor))]
 	class TransparentWalls {
-		static void Postfix(Transform ____obstacleCore) {
-			if(____obstacleCore != null)
+		static void Postfix(Transform ____obstacleCore, ref ParametricBoxFakeGlowController ____obstacleFakeGlow) {
+			if(____obstacleCore != null) {
 				____obstacleCore.gameObject.layer = (int)VisibilityLayers.WallTextures;
+
+				// No-Bloom inner wall texture thingy
+				if(____obstacleFakeGlow.enabled)
+					____obstacleCore.GetChild(0).gameObject.layer = (int)VisibilityLayers.WallTextures;
+			}
+
+			____obstacleFakeGlow.enabled = false;
 		}
 
 		public static void MakeWallsOpaqueForMainCam() {
@@ -16,6 +23,14 @@ namespace Camera2.HarmonyPatches {
 				Plugin.Log.Info("Made walls opaque for main cam!");
 #endif
 				Camera.main.cullingMask |= (int)VisibilityMasks.WallTextures;
+			}
+		}
+		
+		// No-Bloom fake bloom wall edge
+		[HarmonyPatch(typeof(ParametricBoxFakeGlowController), nameof(ParametricBoxFakeGlowController.Awake))]
+		class FunnyNoBloom {
+			static void Postfix(ParametricBoxFakeGlowController __instance) {
+				__instance.gameObject.layer = (int)VisibilityLayers.Walls;
 			}
 		}
 	}
