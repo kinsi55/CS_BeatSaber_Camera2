4c6c2d104ba0863f539f4522aa2120078b0acac5
Kinsi <kinsi55@users.noreply.github.com>
2021-03-13 02:21:39 +0100

    Port Follow360 to Transformer

--- Middlewares/Follow360.cs
+++ Middlewares/Follow360.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using Camera2.HarmonyPatches;
 using Camera2.Interfaces;
 using Camera2.Utils;
+using Camera2.Behaviours;
 
 namespace Camera2.Configuration {
 	class Settings_Follow360 : CameraSubSettings {
@@ -13,15 +14,15 @@ namespace Camera2.Configuration {
 
 namespace Camera2.Middlewares {
 	class Follow360 : CamMiddleware, IMHandler {
+		public void OnDisable() => Reset();
+
+		Transformer rotationApplier = null;
 		float currentRotateAmount = 0f;
-		public void OnDisable() {
-			Reset();
-		}
 
 		private void Reset() {
-			if(currentRotateAmount != 0f) {
-				cam.settings.ApplyPositionAndRotation();
-
+			if(rotationApplier != null) {
+				GameObject.Destroy(rotationApplier);
+				rotationApplier = null;
 				currentRotateAmount = 0f;
 			}
 		}
@@ -42,6 +43,9 @@ namespace Camera2.Middlewares {
 				return true;
 			}
 
+			if(rotationApplier == null)
+				rotationApplier = cam.GetOrCreateTransformer("Follow360", TransformerOrders.Follow360);
+
 			if(HookLevelRotation.Instance.targetRotation != 0f) {
 				// Make sure we dont spam unnecessary calculations / rotation steps for the last little bit
 				if(Math.Abs(currentRotateAmount - HookLevelRotation.Instance.targetRotation) < 1f)
@@ -49,10 +53,17 @@ namespace Camera2.Middlewares {
 
 				var rotateStep = Mathf.LerpAngle(currentRotateAmount, HookLevelRotation.Instance.targetRotation, cam.timeSinceLastRender * settings.Follow360.smoothing);
 
-				cam.transform.RotateAround(
+				rotationApplier.transform.RotateAround(
 					SceneUtil.songWorldTransform != null ? SceneUtil.songWorldTransform.position : Vector3.zero,
 					Vector3.up, (rotateStep - currentRotateAmount)
 				);
+
+				/*
+				 * Firstperson cameras w/ non-pivotingOffset get rotated by this, but since the HMD rotation is
+				 * applied to the camera we dont want to rotate the camera with the map rotation
+				 */
+				if(settings.type != Configuration.CameraType.Positionable)
+					rotationApplier.transform.rotation = Quaternion.identity;
 
 				currentRotateAmount = rotateStep;
 			}
