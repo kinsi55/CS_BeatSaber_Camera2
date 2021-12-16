feecb609c893194348563c4a95dad25d286eaa2d
Kinsi <kinsi55@users.noreply.github.com>
2021-12-16 20:06:58 +0100

    bruh

--- UI/SpaghettiUI.cs
+++ UI/SpaghettiUI.cs
@@ -536,6 +536,9 @@ namespace Camera2.UI {
 
 				_ = IPA.Utilities.Async.UnityMainThreadTaskScheduler.Factory.StartNew(() => {
 					sponsorsText.text = desc;
+					// There is almost certainly a better way to update / correctly set the scrollbar size...
+					sponsorsText.gameObject.SetActive(false);
+					sponsorsText.gameObject.SetActive(true);
 				});
 			}).ConfigureAwait(false);
 		}
