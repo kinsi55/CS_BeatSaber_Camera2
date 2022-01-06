e92341cfadcc8aff3b0199225e733aaa52f0ebe9
Kinsi <kinsi55@users.noreply.github.com>
2022-01-06 17:38:18 +0100

    It was in fact not a better play to init at

--- HarmonyPatches/InitOnMainAvailable.cs
+++ HarmonyPatches/InitOnMainAvailable.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 using System.Linq;
 
 namespace Camera2.HarmonyPatches {
-	[HarmonyPatch(typeof(FirstPersonFlyingController), nameof(FirstPersonFlyingController.Awake))]
+	[HarmonyPatch(typeof(SmoothCameraController), nameof(SmoothCameraController.ActivateSmoothCameraIfNeeded))]
 	static class InitOnMainAvailable {
 		static bool isInited = false;
 		public static bool useDepthTexture { get; private set; }
-		static void Postfix(Camera ____camera) {
-			useDepthTexture = ____camera.depthTextureMode != DepthTextureMode.None;
+		static void Postfix(MainSettingsModelSO ____mainSettingsModel) {
+			useDepthTexture = ____mainSettingsModel.smokeGraphicsSettings;
 
 			if(!isInited) {
 				if(CamManager.baseCullingMask == 0)
@@ -17,16 +17,13 @@ namespace Camera2.HarmonyPatches {
 
 				isInited = true;
 
-				Plugin.Log.Notice("MainCamera is ready, Initializing...");
+				Plugin.Log.Notice("Game is ready, Initializing...");
 
 				CamManager.Init();
 			} else {
 				foreach(var cam in CamManager.cams.Values)
 					cam.UpdateDepthTextureActive();
 			}
-
-			if(!HookFPFCToggle.foundSiraToggle)
-				HookFPFCToggle.SetFPFCActive(____camera.transform, ____camera.isActiveAndEnabled);
 		}
 	}
 }
