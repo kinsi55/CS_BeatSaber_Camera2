7b240e78b182377e68842c60e80188fff8f3cf5c
Kinsi <kinsi55@users.noreply.github.com>
2022-03-15 01:12:28 +0100

    1.20

--- HarmonyPatches/HookLeveldata.cs
+++ HarmonyPatches/HookLeveldata.cs
@@ -15,8 +15,6 @@ namespace Camera2.HarmonyPatches {
 		public static bool isModdedMap = false;
 		public static bool isWallMap = false;
 
-		static SpawnRotationProcessor spawnRotationProcessor = new SpawnRotationProcessor();
-
 		[HarmonyPriority(int.MinValue)]
 		[HarmonyPatch(typeof(StandardLevelScenesTransitionSetupDataSO), nameof(StandardLevelScenesTransitionSetupDataSO.Init))]
 		[HarmonyPatch(typeof(MissionLevelScenesTransitionSetupDataSO), nameof(MissionLevelScenesTransitionSetupDataSO.Init))]
@@ -29,25 +27,22 @@ namespace Camera2.HarmonyPatches {
 			HookLeveldata.gameplayModifiers = gameplayModifiers;
 
 			isModdedMap = ModMapUtil.IsModdedMap(difficultyBeatmap);
-			is360Level = difficultyBeatmap?.beatmapData?.beatmapEventsData.Any(
-				x => x.type.IsRotationEvent() && spawnRotationProcessor.RotationForEventValue(x.value) != 0f
-			) == true;
+			is360Level = false;
 			isWallMap = ModMapUtil.IsProbablyWallmap(difficultyBeatmap);
 		}
 
 
-		//[HarmonyPatch(typeof(GameplayCoreInstaller), "InstallBindings")]
-		//static class threesixtycheck {
-		//	// TODO: remove optional thing next update
-		//	static bool Prepare() => UnityGame.GameVersion > new AlmostVersion("1.19.1");
-			
-		//	// this is dumb this is dumb this is dumbis is dumb this is dumb this is dumb why GetBeatmapDataAsync() why hyhwhy
-		//	static void Postfix(GameplayCoreSceneSetupData ____sceneSetupData) {
-		//		is360Level = ____sceneSetupData.transformedBeatmapData.GetBeatmapDataItems<SpawnRotationBeatmapEventData>().Any(
-		//			x => x.rotation != 0f
-		//		) == true;
-		//	}
-		//}
+		[HarmonyPatch(typeof(GameplayCoreInstaller), "InstallBindings")]
+		static class threesixtycheck {
+			// TODO: remove optional thing next update
+			static bool Prepare() => UnityGame.GameVersion > new AlmostVersion("1.19.1");
+
+			static void Postfix(GameplayCoreSceneSetupData ____sceneSetupData) {
+				is360Level = ____sceneSetupData.transformedBeatmapData.allBeatmapDataItems.Any(
+					x => x.type == BeatmapDataItem.BeatmapDataItemType.BeatmapEvent && x is SpawnRotationBeatmapEventData sr && sr.rotation != 0f
+				) == true;
+			}
+		}
 
 		internal static void Reset() {
 			is360Level = isModdedMap = isWallMap = false;
