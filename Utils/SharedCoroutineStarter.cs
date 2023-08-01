9b596886e45620f3b84520bad5e68274649e91cf
Kinsi <kinsi55@users.noreply.github.com>
2023-08-02 00:19:52 +0200

    1.31

---
+++ Utils/SharedCoroutineStarter.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class SharedCoroutineStarter : MonoBehaviour {
+	static MonoBehaviour _instance = null;
+	public static MonoBehaviour instance => _instance ??= new GameObject().AddComponent<SharedCoroutineStarter>();
+
+	void Awake() {
+		GameObject.DontDestroyOnLoad(gameObject);
+	}
+}
