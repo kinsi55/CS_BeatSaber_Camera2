52523e6e16ec39741c800fbb91a74bcb1299c89e
qqrz997 <156676353+qqrz997@users.noreply.github.com>
2024-10-08 21:11:12 +0100

    depth texture fix

---
+++ HarmonyPatches/HookSettingsManager.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+
+namespace Camera2.HarmonyPatches {
+	[HarmonyPatch(typeof(DepthTextureController), nameof(DepthTextureController.Init))]
+	static class HookSettingsManager {
+		public static SettingsManager settingsManager { get; private set; }
+		public static bool useDepthTexture { get; private set; }
+		static void Postfix(SettingsManager settingsManager) {
+			HookSettingsManager.settingsManager = settingsManager;
+			useDepthTexture = settingsManager.settings.quality.smokeGraphics;
+		}
+	}
+}
